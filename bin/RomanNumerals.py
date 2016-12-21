4bb40b24fc31a99d4972fca9e90a5912fd3e5ee7
Luke Bodeen <luke_Bodeen@yahoo.com>
2016-12-21 13:36:15 -0600

    added conf, also error handled for non-whole numbers

--- bin/RomanNumerals.py
+++ bin/RomanNumerals.py
@@ -1,12 +1,27 @@
-
-
+import configparser
 
 
 class RomanNumerals:
     
+    def __init__(self):
+        numeralConfig = configparser.ConfigParser()
+        numeralConfig.read("../conf/NumeralMapping.conf")
+        print(numeralConfig['NUMERAL_MAPPING']["1"])
+        
+    
     def convert_number_to_numeral(self, inputNumber):
-        print(inputNumber)
+      
+        try: 
+            testval = int(inputNumber)
+            strInputNumber = str(inputNumber)
+        except ValueError:
+            return "Non-number or non-whole number passed in, please re-try with whole number"
         
+        inputNumberLength = len(strInputNumber)
         
+        print(inputNumberLength)        
+        
+        if(inputNumberLength == 1):
+            print("single digit")
         
         return "I"
--- test/RomanNumeralsTest.py
+++ test/RomanNumeralsTest.py
@@ -6,20 +6,23 @@ from bin.RomanNumerals import RomanNumerals
 class TestRomanNumerals(unittest.TestCase):
     
     def setUp(self):
-        print("setup")
         self.romanInstance = RomanNumerals()
         pass
     
     def testReturnNumeralIFromNumberOne(self):
-        print("test 1")
         self.assertEqual(self.romanInstance.convert_number_to_numeral(1), "I", "I (string) not returned")
         
+    def testReturnMessageIfInputLetter(self):
+        self.assertEqual(self.romanInstance.convert_number_to_numeral("a"), "Non-number or non-whole number passed in, please re-try with whole number", "Non-number allowed through") 
+     
+    def testReturnMessageIfInputSymbol(self):
+        self.assertEqual(self.romanInstance.convert_number_to_numeral("~"), "Non-number or non-whole number passed in, please re-try with whole number", "Non-number allowed through")           
+        
     def testReturnNumeralFromSingleDigitsNumber(self):
-        print("test 1")
         self.assertEqual(self.romanInstance.convert_number_to_numeral(1), "I", "I (string) not returned")        
 
 
 if __name__ == "__main__":
-    print("main")
     unittest.main()
+    
     
