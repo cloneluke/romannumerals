ce56a67f390f61e8b6228a85bb073fa0b74577ea
Luke Bodeen <luke_Bodeen@yahoo.com>
2016-12-21 11:57:38 -0600

    fixed with first test and new numerals class

---
+++ bin/RomanNumerals.py
@@ -0,0 +1,10 @@
+
+
+
+
+class RomanNumerals:
+    
+    def convert_numeral_to_number(self, RomanString):
+        print(RomanString)
+        
+        return 1
--- test/RomanNumeralsTest.py
+++ test/RomanNumeralsTest.py
@@ -1,13 +1,21 @@
 import unittest
-import romannumerals
+import bin.RomanNumerals
+from bin.RomanNumerals import RomanNumerals
 
 
 class TestRomanNumerals(unittest.TestCase):
     
-    def ReturnNumberFromString(self):
-        self.assertEqual(romannumerals.convert_numeral_to_number("test"), 1, "Number not returned")
+    def setUp(self):
+        print("setup")
+        pass
+    
+    def testReturnNumberFromString(self):
+        print("test 1")
+        romanInstance = RomanNumerals()
+        self.assertEqual(romanInstance.convert_numeral_to_number("test"), 1, "Number not returned")
 
 
 if __name__ == "__main__":
+    print("main")
     unittest.main()
     
