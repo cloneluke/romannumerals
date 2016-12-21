9eec351fc17e2650ae3b02350767e835a475f401
Luke Bodeen <luke_Bodeen@yahoo.com>
2016-12-21 15:06:38 -0600

    broke out input validation into its own method, adjusted tests to catch runtime errors

--- bin/RomanNumerals.py
+++ bin/RomanNumerals.py
@@ -9,8 +9,7 @@ class RomanNumerals:
         print(numeralConfig['NUMERAL_MAPPING']["1"])
         
     
-    def convert_number_to_numeral(self, inputNumber):
-
+    def error_handle_input_postive_whole_numbers_only(self, inputNumber):
         #Handle if input is string, we will allow if its a postive whole number
         #any other strings not allowed      
         if(isinstance(inputNumber, str)):
@@ -19,17 +18,24 @@ class RomanNumerals:
                 strInputNumber = str(inputNumber)
                 print("converted input:" + strInputNumber)
             except ValueError:
-                return "Non-number passed in, please re-try with whole number"
+                raise RuntimeError("Non-number passed in, please re-try with whole number")
          
         #after string handling check if its a whole number         
         if(inputNumber % 1 != 0):
-            return "Decimal passed in, please re-try with whole number"
+            raise RuntimeError("Decimal passed in, please re-try with whole number")
         #after whole number handling, check if its greater than zero
         if(inputNumber < 1):
-            return "Input number has to be greater than or equal to one"
-              
+            raise RuntimeError("Input number has to be greater than or equal to one")
+        
         strInputNumber = str(inputNumber)   
         
+        return strInputNumber
+        
+    
+    def convert_number_to_numeral(self, inputNumber):
+        
+        strInputNumber = self.error_handle_input_postive_whole_numbers_only(inputNumber)  
+                  
         inputNumberLength = len(strInputNumber)
         
         print("input length: " + str(inputNumberLength))        
--- test/RomanNumeralsTest.py
+++ test/RomanNumeralsTest.py
@@ -1,5 +1,5 @@
 import unittest
-import bin.RomanNumerals
+import sys
 from bin.RomanNumerals import RomanNumerals
 
 
@@ -16,22 +16,40 @@ class TestRomanNumerals(unittest.TestCase):
         self.assertEqual(self.romanInstance.convert_number_to_numeral("1"), "I", "I (string) not returned")    
         
     def testReturnMessageIfInputLetter(self):
-        self.assertEqual(self.romanInstance.convert_number_to_numeral("a"), "Non-number passed in, please re-try with whole number", "Non-number allowed through") 
+        try:
+            self.romanInstance.convert_number_to_numeral("a")
+        except RuntimeError as e:
+            self.assertEqual(e.args[0], "Non-number passed in, please re-try with whole number", "Non-number allowed through")
      
     def testReturnMessageIfInputSymbol(self):
-        self.assertEqual(self.romanInstance.convert_number_to_numeral("~"), "Non-number passed in, please re-try with whole number", "Non-number allowed through") 
+        try:
+            self.romanInstance.convert_number_to_numeral("~")
+        except RuntimeError as e: 
+            self.assertEqual(e.args[0], "Non-number passed in, please re-try with whole number", "Non-number allowed through") 
     
     def testReturnMessageIfInputDecimal(self):
-        self.assertEqual(self.romanInstance.convert_number_to_numeral(20.03), "Decimal passed in, please re-try with whole number", "Decimal allowed through")
+        try:
+            self.romanInstance.convert_number_to_numeral(20.03)  
+        except RuntimeError as e:
+            self.assertEqual(e.args[0], "Decimal passed in, please re-try with whole number", "Decimal allowed through")
     
     def testReturnMessageIfInputDecimalString(self):
-        self.assertEqual(self.romanInstance.convert_number_to_numeral("20.03"), "Decimal passed in, please re-try with whole number", "Decimal allowed through")    
+        try:
+            self.romanInstance.convert_number_to_numeral("20.03")
+        except RuntimeError as e:
+            self.assertEqual(e.args[0], "Decimal passed in, please re-try with whole number", "Decimal allowed through")    
     
     def testReturnMessageIfInputNegativeWhole(self):
-        self.assertEqual(self.romanInstance.convert_number_to_numeral(-2), "Input number has to be greater than or equal to one", "Zero or negative allowed through")  
+        try:
+            self.romanInstance.convert_number_to_numeral(-2)
+        except RuntimeError as e:
+            self.assertEqual(e.args[0], "Input number has to be greater than or equal to one", "Zero or negative allowed through")  
      
     def testReturnMessageIfInputNegativeWholeString(self):
-        self.assertEqual(self.romanInstance.convert_number_to_numeral("-2"), "Input number has to be greater than or equal to one", "Zero or negative allowed through")                   
+        try:
+            self.romanInstance.convert_number_to_numeral("-2")
+        except RuntimeError as e:   
+            self.assertEqual(e.args[0], "Input number has to be greater than or equal to one", "Zero or negative allowed through")                   
         
     def testReturnNumeralFromSingleDigitsNumber(self):
         self.assertEqual(self.romanInstance.convert_number_to_numeral(1), "I", "I (string) not returned")        
